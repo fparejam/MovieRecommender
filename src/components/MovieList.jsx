a5b407cf3a08249669b9d04409677bee317a9227
fparejam <fparejamayo@gmail.com>
2023-05-04 21:21:48 +0200

    greatly changed frontend view with bootstrap styles

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,75 +1,23 @@
 import React from 'react';
-import MovieCard from './MovieCard';
-import Carousel from 'react-multi-carousel';
-import 'react-multi-carousel/lib/styles.css';
-import styled from 'styled-components';
+import styles from '../styles/MovieList.module.css'
 
-const MovieListContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin: 0 auto;
-  padding: 20px;
-  text-align: center;
-`;
-
-const Header = styled.h1`
-  font-weight: 600;
-  color: #333;
-`;
-
-
-const MovieList = ({ movies, setSelectedMovie }) => {
-  const responsive = {
-    superLargeDesktop: {
-      breakpoint: { max: 4000, min: 3000 },
-      items: 7,
-    },
-    desktop: {
-      breakpoint: { max: 3000, min: 1024 },
-      items: 7,
-    },
-    tablet: {
-      breakpoint: { max: 1024, min: 464 },
-      items: 3,
-    },
-    mobile: {
-      breakpoint: { max: 464, min: 0 },
-      items: 1,
-    },
-  };
-
-  // Filter top 20 most popular movies based on IMDb rating
-  const top20Movies = movies
-    .slice(0, 20);
-
-  console.log('top20Movies:', top20Movies);
-
-  return (
-    <MovieListContainer>
-      <Header>Most Popular Movies</Header>
-      <Carousel
-        swipeable={true}
-        draggable={true}
-        showDots={false}
-        responsive={responsive}
-        ssr={true}
-        infinite={true}
-        autoPlay={false}
-        keyBoardControl={true}
-        customTransition="all .5"
-        transitionDuration={500}
-        containerClass="carousel-container"
-        removeArrowOnDeviceType={['tablet', 'mobile']}
-        dotListClass="custom-dot-list-style"
-        itemClass="carousel-item-padding-40-px"
-      >
-        {top20Movies.map((movie) => (
-          <MovieCard key={movie.Title} movie={movie} setSelectedMovie={setSelectedMovie} />
-        ))}
-      </Carousel>
-    </MovieListContainer>
-  );
+const MovieList = (props) => {
+	const FavouriteComponent = props.favouriteComponent;
+	return (
+		<>
+			{props.movies.map((movie, index) => (
+				<div className={`${styles.image_container}  d-flex justify-content-start m-3`} key={movie.imdbID}>
+					<img src={movie.Poster} alt='movie' className={styles.image} ></img>
+					<div
+						onClick={() => props.handleFavouritesClick(movie)}
+						className={`${styles.overlay} d-flex align-items-center justify-content-center`}
+					>
+						<FavouriteComponent />
+					</div>
+				</div>
+			))}
+		</>
+	);
 };
 
 export default MovieList;
