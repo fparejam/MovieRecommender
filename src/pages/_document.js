0c4d0f843d0f419f1d3ebf2f321d4355f9abd848
fparejam <fparejamayo@gmail.com>
2023-05-11 02:22:11 +0200

    Changed favicon to Netflix style

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+          <title>Netflix</title>
+          <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
       <body>
         <Main />
         <NextScript />
